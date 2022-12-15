dc3f6df69eca12c02237c27e1fa3f9a292585091
Pawel Skiers <pawelsk269@gmail.com>
2022-12-15 22:09:03 +0100

    Add QT library configuration

--- app/artificial_life.cc
+++ app/artificial_life.cc
@@ -1,14 +1,22 @@
 #include "game.h"
 #include "herbivore.h"
+#include "./include/mainwindow.h"
 
 #include <iostream>
 
+#include <QApplication>
+
 using namespace std;
 
-int main() {
+
+int main(int argc, char *argv[])
+{
     Game game( 1, 2, 3, 4 );
     Field f;
     Map m( 2, 3 );
-    cout << "DUPA Artificial life game DUPA\n";
-    return 0;
+
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
